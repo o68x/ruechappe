0d9391baa9a6dcf34218f39f54137b9cac953bc7
Olivier Cardinaux <ocardinaux@gmail.com>
2021-12-16 21:09:49 +0100

    fix: back to scss

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,9 @@ export default (
     dist: './dist',       // When running `astro build`, path to final static output
     public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
     buildOptions: {
-      // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-      sitemap: false,         // Generate sitemap (set to "false" to disable)
-      sourcemap: false,
+      site: 'https://elated-mestorf-cbbfa4.netlify.app/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+      sitemap: true,         // Generate sitemap (set to "false" to disable)
+      sourcemap: true,
     },
     devOptions: {
       // hostname: 'localhost',  // The hostname to run the dev server on.
